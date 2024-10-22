546aedf4482b00f7981edca26d36336a2568b69f
pthrr <pthrr@posteo.de>
2024-10-23 01:34:20 +0200

    Added enums + Fixes

--- consts.cpp
+++ consts.cpp
@@ -4,21 +4,35 @@
 namespace my_ns {
 
 constexpr std::array< int, 3 > g_MY_ARR1 = { 1, 2, 3 }; // global, compile-time, compound
-const std::array< int, 3 > g_MY_ARR2 = { 4, 5, 6 };     // global, run-time, compound
+std::array< int, 3 > const g_MY_ARR2 = { 4, 5, 6 };     // global, run-time, compound
 
 constexpr int g_MY_INT1 = 7; // global, compile-time, integral
-const int g_MY_INT2 = 8;     // global, run-time, integral
+int const g_MY_INT2 = 8;     // global, run-time, integral
 
 struct MyClass
 {
     static constexpr std::array< int, 3 > s_MY_ARR1 = { 1, 2, 3 }; // class, compile-time, compound
-    static const std::array< int, 3 > s_MY_ARR2;                   // class, run-time, compound
+    static std::array< int, 3 > const s_MY_ARR2;                   // class, run-time, compound
 
     static constexpr int s_MY_INT1 = 7; // class, compile-time, integral
-    static const int s_MY_INT2 = 8;     // class, run-time, integral
+    static int const s_MY_INT2 = 8;     // class, run-time, integral
+
+    auto print() -> void
+    {
+        for( auto const x : s_MY_ARR1 ) {
+            std::printf( "%d\n", x );
+        }
+
+        for( auto const x : s_MY_ARR2 ) {
+            std::printf( "%d\n", x );
+        }
+
+        std::printf( "%d\n", s_MY_INT1 );
+        std::printf( "%d\n", s_MY_INT2 );
+    }
 };
 
-const std::array< int, 3 > MyClass::s_MY_ARR2 = { 4, 5, 6 };
+std::array< int, 3 > const MyClass::s_MY_ARR2 = { 4, 5, 6 };
 
 }
 
@@ -36,15 +50,5 @@ auto main() -> int
     std::printf( "%d\n", my_ns::g_MY_INT2 );
 
     my_ns::MyClass m;
-
-    for( auto const x : m.s_MY_ARR1 ) {
-        std::printf( "%d\n", x );
-    }
-
-    for( auto const x : m.s_MY_ARR2 ) {
-        std::printf( "%d\n", x );
-    }
-
-    std::printf( "%d\n", m.s_MY_INT1 );
-    std::printf( "%d\n", m.s_MY_INT2 );
+    m.print();
 }
---
+++ enums.cpp
@@ -0,0 +1,71 @@
+#include <cstdint>
+#include <print>
+#include <stdexcept>
+#include <string>
+#include <utility> // for std::to_underlying
+
+enum struct Permission : uint8_t
+{
+    NONE = 0,
+    READ = 1 << 0,
+    WRITE = 1 << 1,
+    EXECUTE = 1 << 2,
+    ALL = READ | WRITE | EXECUTE,
+};
+
+constexpr auto operator|( Permission lhs, Permission rhs ) -> Permission
+{
+    return static_cast< Permission >( std::to_underlying( lhs ) | std::to_underlying( rhs ) );
+}
+
+constexpr auto operator|=( Permission& lhs, Permission rhs ) -> Permission&
+{
+    lhs = lhs | rhs;
+    return lhs;
+}
+
+constexpr auto operator==( Permission lhs, Permission rhs ) -> bool
+{
+    return std::to_underlying( lhs ) == std::to_underlying( rhs );
+}
+
+constexpr std::array< std::pair< Permission, std::string_view >, 5 > permission_to_string = {
+    { { Permission::NONE, "None" }, { Permission::READ, "Read" }, { Permission::WRITE, "Write" },
+        { Permission::EXECUTE, "Execute" }, { Permission::ALL, "All" } } };
+
+auto toString( Permission perm ) -> std::string
+{
+    std::string result;
+    bool first = true;
+
+    for( const auto& pair : permission_to_string ) {
+        if( pair.first != Permission::ALL && std::to_underlying( perm ) & std::to_underlying( pair.first ) ) {
+            if( not first ) {
+                result += " | ";
+            }
+
+            result += pair.second;
+            first = false;
+        }
+    }
+
+    return result.empty() ? "Unknown" : result;
+}
+
+constexpr auto fromStr( char const* name ) -> Permission
+{
+    for( auto const& pair : permission_to_string ) {
+        if( pair.second == std::string_view( name ) ) {
+            return pair.first;
+        }
+    }
+
+    throw std::invalid_argument( "Unknown enum value" );
+}
+
+int main()
+{
+    constexpr Permission perm = fromStr( "Read" ) | fromStr( "Write" );
+    std::printf( "%s\n", toString( perm ).c_str() );
+    return 0;
+}
