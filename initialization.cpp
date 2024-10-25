f93f56d142903b601a8932ed478339f7a92b2ba0
pthrr <pthrr@posteo.de>
2024-10-25 15:33:14 +0200

    Added init + Fixes

---
+++ initialization.cpp
@@ -0,0 +1,42 @@
+#include <print>
+#include <string>
+#include <vector>
+
+using namespace std::literals::string_literals;
+
+struct Widget
+{
+    template< typename T >
+        requires std::is_same_v< std::decay_t< T >, std::string >
+    Widget( T&& name, float price, int quantity )
+        : m_quantity( quantity )
+        , m_price( price )
+        , m_name( std::forward< T >( name ) )
+    {
+    }
+
+    auto getName() const -> std::string
+    {
+        return m_name;
+    }
+
+private:
+    int m_quantity;
+    float m_price;
+    std::string m_name;
+};
+
+auto main() -> int
+{
+    int i = 0;
+    std::vector< int > v = { 1, 2, 3, 4 };
+    auto [name, price, quantity] = std::make_tuple( "abc"s, 3.2f, i );
+    auto w1 = Widget( name, price, quantity );
+    std::printf( "%s\n", w1.getName().c_str() );
+    name = "changed"s;
+    auto w2 = Widget( std::move( name ), price, quantity );
+    std::printf( "%s\n", w2.getName().c_str() );
+    auto w3 = Widget( std::string( "new" ), price, quantity );
+    std::printf( "%s\n", w3.getName().c_str() );
+    return 0;
+}
